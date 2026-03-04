8c887f33128e3f41af1a2e0e0baf3024151bc31a
Eduardo Palacios <120128932+EdPalace@users.noreply.github.com>
2026-03-04 15:28:53 -0600

    updating main.dart and add firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:flutter/material.dart';
 import 'package:emergency_response_app/core/services/services.dart';
 import 'app.dart';
@@ -6,5 +9,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await IsarService.init();
   //await FireBaseService.init();    when Firebase is ready
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,);
+
   runApp(const MyApp());
 }
